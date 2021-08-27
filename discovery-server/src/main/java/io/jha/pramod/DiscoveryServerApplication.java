5adf0a16b0bb3e8b06ea59f36ac26ccb6da033e3
sg0959571 <pramod.kumar@sabre.com>
2021-08-27 15:59:22 +0530

    discovery-server changes

--- discovery-server/src/main/java/io/jha/pramod/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/io/jha/pramod/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package io.schultz.dustin;
+package io.jha.pramod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- discovery-server/src/test/java/io/jha/pramod/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/io/jha/pramod/DiscoveryServerApplicationTests.java
@@ -1,4 +1,4 @@
-package io.schultz.dustin;
+package io.jha.pramod;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
