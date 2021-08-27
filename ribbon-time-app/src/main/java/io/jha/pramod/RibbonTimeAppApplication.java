1347ae67dc56132b32305b5523da4f4a22df2ed3
sg0959571 <pramod.kumar@sabre.com>
2021-08-27 16:11:52 +0530

    Load balancing using Ribbon with service discovery feature

---
+++ ribbon-time-app/src/main/java/io/jha/pramod/RibbonTimeAppApplication.java
@@ -0,0 +1,38 @@
+package io.jha.pramod;
+
+import javax.inject.Inject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RibbonTimeAppApplication {
+	@Inject
+	private RestTemplate restTemplate;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonTimeAppApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTeplate() {
+		return new RestTemplate();
+
+	}
+	
+	@GetMapping
+	public String getTime() {
+		return restTemplate.getForEntity("http://time-service", String.class).getBody();
+
+	}
+
+}
