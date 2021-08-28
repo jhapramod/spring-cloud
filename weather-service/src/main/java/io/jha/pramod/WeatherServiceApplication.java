af18ca98541e4b1028bce9e960f9757842dfb779
sg0959571 <pramod.kumar@sabre.com>
2021-08-28 10:58:53 +0530

    Implementing fault tolerance using Netflix Hystrix

--- weather-service/src/main/java/io/jha/pramod/WeatherServiceApplication.java
+++ weather-service/src/main/java/io/jha/pramod/WeatherServiceApplication.java
@@ -1,13 +1,27 @@
 package io.jha.pramod;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WeatherServiceApplication {
 
+	private String[] weather = new String[] {"rainy", "sunny", "cloudy", "windy"};
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherServiceApplication.class, args);
+	}
+	
+	@GetMapping("weather")
+	public String getWeather() {
+		int rand = ThreadLocalRandom.current().nextInt(0, 4);
+		return weather[rand];
 	}
 
 }
